1ef0ca9a9ae50c222d2d9e01bfada8ce1c648079
tarunsas <sankar.sriram@gmail.com>
2011-08-06 17:26:41 -0500

    all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pharmaconsult::Application.config.secret_token = 'c7719053f760392c8179e77efcb29996962c9b750e9a65b155dbbf73d6b28bf3aaaab1ddd13b62a40516ba855acbcb924be2bc76812bb569505d18134e5aca85'
